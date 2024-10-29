c71c9ae35e9360f1f692ffbd020a1e0fb1899d29
Bovis ALOUKOU <bovisaloukou@gmail.com>
2024-10-29 11:06:47 +0100

    Récupération du token

--- src/components/ConfessionList.js
+++ src/components/ConfessionList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import jwt_decode from 'jwt-decode';
 
 const ConfessionList = () => {
     const [confessions, setConfessions] = useState([]);
